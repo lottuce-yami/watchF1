4b3ece2c18a76580415af55960ce4ae4fdb57193
Lotte <lottuce.yami@gmail.com>
2023-10-06 10:47:54 +0200

    Optimize videos accessing by switching to server-side query evaluation

---
+++ Data/QueryExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq.Expressions;
+
+namespace F1Project.Data;
+
+public static class QueryExtensions
+{
+    public static IQueryable<T> WhereIf<T>(this IQueryable<T> query, bool condition, Expression<Func<T, bool>> whereClause)
+    {
+        return condition ? query.Where(whereClause) : query;
+    }
+}
