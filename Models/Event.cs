6f182306e43e641bc2cc4853c58aa1a2f0d32b0e
Lotte <lottuce.yami@gmail.com>
2023-10-12 21:43:32 +0200

    Change Event primary key to new numeric Id property

--- Models/Event.cs
+++ Models/Event.cs
@@ -7,12 +7,15 @@ namespace F1Project.Models;
 public partial class Event
 {
     [Key]
-    [Column("time")]
-    public DateTime Time { get; set; }
-
+    [Column("id")]
+    public short Id { get; set; }
+    
     [Column("title")]
     [StringLength(128)]
     public string Title { get; set; } = null!;
+    
+    [Column("time")]
+    public DateTime Time { get; set; }
 
     [Column("featured")]
     public bool? Featured { get; set; }
