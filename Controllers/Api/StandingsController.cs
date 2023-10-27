55d794af4d132dda0569f0774fe3708032d17505
Lotte <lottuce.yami@gmail.com>
2023-10-27 12:24:17 +0200

    Implement getting standings entities by id in API

--- Controllers/Api/StandingsController.cs
+++ Controllers/Api/StandingsController.cs
@@ -1,4 +1,5 @@
 ﻿using F1Project.Data;
+using F1Project.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace F1Project.Controllers.Api;
@@ -12,5 +13,27 @@ public class StandingsController : ControllerBase
     public StandingsController(WatchF1Context context)
     {
         _context = context;
+    }
+    
+    [HttpGet("drivers/{id}")]
+    public async Task<ActionResult<Driver>> GetDriver(string id)
+    {
+        var driver = await _context.FindAsync<Driver>(id);
+
+        if (driver == null) 
+            return NotFound();
+
+        return driver;
+    }
+
+    [HttpGet("constructors/{id}")]
+    public async Task<ActionResult<Constructor>> GetConstructor(string id)
+    {
+        var constructor = await _context.FindAsync<Constructor>(id);
+
+        if (constructor == null) 
+            return NotFound();
+
+        return constructor;
     }
 }
