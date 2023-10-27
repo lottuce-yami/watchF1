d8cb4b344f4b9d12afbb85912cfec812cc61c6e8
Lotte <lottuce.yami@gmail.com>
2023-10-27 13:53:19 +0200

    Add current Grand Prix option to appsettings.json

--- Data/Options/MiscOptions.cs
+++ Data/Options/MiscOptions.cs
@@ -4,6 +4,7 @@ public class MiscOptions
 {
     public const string Misc = "Misc";
 
+    public short CurrentGrandPrix { get; set; }
     public bool StandingsToggle { get; set; }
     public bool TestMode { get; set; }
 }
