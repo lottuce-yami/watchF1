739c396dfa6c67296a566c73158c278e637ba06d
Lotte <lottuce.yami@gmail.com>
2023-10-27 10:30:10 +0200

    Implement getting video by id in API

--- Controllers/Api/VideosController.cs
+++ Controllers/Api/VideosController.cs
@@ -1,4 +1,5 @@
 ﻿using F1Project.Data;
+using F1Project.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace F1Project.Controllers.Api;
@@ -12,5 +13,16 @@ public class VideosController : ControllerBase
     public VideosController(WatchF1Context context)
     {
         _context = context;
+    }
+    
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Video>> Get(string id)
+    {
+        var video = await _context.FindAsync<Video>(id);
+
+        if (video == null) 
+            return NotFound();
+
+        return video;
     }
 }
